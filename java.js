91e73dca7f735fa76f5404c5e616303fabcd1a93
unknown <guibender_@hotmail.com>
2021-09-07 16:45:45 -0300

    mudei o codigo

--- java.js
+++ java.js
@@ -2,3 +2,11 @@ function scrollWin(x, y) {
   window.scrollBy(x, y);
   
 }
+
+var mensagem = document.querySelector(".botao");
+
+function alerta(){
+    alert("Obrigado pelo FeedBack");
+}
+
+mensagem.onclick = alerta;
